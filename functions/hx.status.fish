87fc90a09612fac3a6c641d03cd1a304210b89dd
kpbaks <kristoffer.pbs@tuta.io>
2024-08-27 16:24:43 +0200

    feat(hx.status): show `hx --version` and compare it with latest upstream

--- functions/hx.status.fish
+++ functions/hx.status.fish
@@ -12,14 +12,32 @@ function hx.status
 
     set -l reset (set_color normal)
     set -l yellow (set_color yellow)
+    set -l red (set_color red)
+    set -l green (set_color green)
     set -l blue (set_color blue)
     set -l b (set_color --bold)
+    set -l i (set_color --italics)
+    set -l dim (set_color --dim)
     set -l pid_color (set_color --background yellow '#000000')
 
     if test (count $hx_pids) -eq 0
         printf 'No %shx%s processes are running\n' (set_color $fish_color_command) $reset
         return
     end
+
+    set -l cache_dir $__fish_user_data_dir/helix.fish
+    test -d $cache_dir; or command mkdir $cache_dir; or return
+
+    # Fetch version of latest release from github, and cache it on disk. If not the newest the notify user
+    set -l releases_url https://api.github.com/repos/helix-editor/helix/releases/latest
+    set -l latest_hx_version $cache_dir/latest_hx_version
+    if not test -f $latest_hx_version; or test (path mtime --relative $latest_hx_version) -gt 86400
+        echo "downloading version ..."
+        command curl -s $releases_url | string match --regex --groups-only '"tag_name": "(.+)"' >$latest_hx_version
+    end
+    set -l hx_latest_version_parts (string split '.' <$cache_dir/latest_hx_version)
+    # echo $latest_hx_version_parts
+    # return
 
     set -l CLK_TCK (getconf CLK_TCK)
     # START_TIME=$(awk '{print $22}' /proc/[PID]/stat)
@@ -38,14 +56,43 @@ function hx.status
         printf '  - %sexe%s:          %s%s%s\n' $b $reset (set_color $fish_color_command) (path resolve /proc/$pid/exe) $reset
         printf '  - %scwd%s:          %s%s%s\n' $b $reset (set_color $fish_color_cwd) (path resolve /proc/$pid/cwd) $reset
         printf '  - %scmdline%s:      %s%s\n' $b $reset (printf (echo (string split0 </proc/$pid/cmdline) | fish_indent --ansi)) $reset
+
         printf '  - %scpu%s:          todo\n' $b $reset
+
         printf '  - %sram%s:          todo\n' $b $reset
+
         # IDEA: this is to see which lsps it is running
         printf '  - %ssubprocesses%s: todo\n' $b $reset
-        # TODO: fetch version of latest release from github, and cache it on disk. If not the newest the notify user
-        printf '  - %sversion%s:      %s\n' $b $reset (/proc/$pid/exe --version | string split ' ' --fields=2)
+
+        # helix version
+        begin
+            set -l hx_version (/proc/$pid/exe --version | string split ' ' --fields=2)
+            set -l hx_version_parts (string split '.' -- $hx_version)
+
+            set -l nparts (math max "$(count $hx_version_parts),$(count $hx_latest_version_parts)")
+            if test (count $hx_version_parts) -lt $nparts
+                set -a hx_version_parts (for i in (seq (math "$nparts - $(count $hx_version_parts)")); echo 0; end)
+            end
+            if test (count $hx_latest_version_parts) -lt $nparts
+                set -a hx_latest_version_parts (for i in (seq (math "$nparts - $(count $hx_latest_version_parts)")); echo 0; end)
+            end
+
+            set -l newer_version_available 0
+            for i in (seq $nparts)
+                if test $hx_version_parts[$i] -lt $hx_latest_version_parts[$i]
+                    set newer_version_available 1
+                    break
+                end
+            end
+
+            printf '  - %sversion%s:      ' $b $reset
+            if test $newer_version_available -eq 1
+                printf '%s%s%s %sa newer release version is available %s%s%s%s\n' $red $hx_version $reset $dim $reset (set_color green --dim) (cat $latest_hx_version) $reset
+            else
+                printf '%s%s%s %snewest release version%s\n' $green $hx_version $reset $dim $reset
+            end
+        end
         # IDEA: walk the process tree, using the pids ppid until we hit pid=0, on the way look for known terminals like `kitty` or `alacritty`
-        # set -l terminal
         begin
             set -l ppid $pid
             while true
@@ -57,7 +104,7 @@ function hx.status
                 # echo "exe: $exe"
                 # echo "name: $name"
                 switch $name
-                    case kitty alacritty konsole
+                    case kitty alacritty konsole zellij tmux
                         set -f terminal (path resolve /proc/$ppid/exe)
                         break
                     case '*'
@@ -73,13 +120,6 @@ function hx.status
                 printf '%s UNKNOWN %s\n' (set_color --background brred '#000000') $reset
             end
         end
-        # set -l ppids $pid
-        # while test $ppids[1] != 0
-        #     set -l pid $ppids[1]
-        #     set --prepend ppids $ppid
-        #     # printf 'ppid: %s exe: %s\n' $ppid (path resolve /proc/$ppid/exe)
-        # end
-
 
         # IDEA: it must be possible to list which fds the process has open or which files it has mmapped into its virtual memory
         printf '  - %sconfig.toml%s:  todo\n' $b $reset
